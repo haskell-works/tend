961cd4c59c2e1bce387c26ed246fd823a5fa5589
John Ky <newhoggy@gmail.com>
2017-02-22 23:15:40 +1100

    Get remotes

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,8 @@
 module Main where
 
 import Control.Lens
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 import Data.Aeson
 import Data.Map
 import Data.Text.Lazy
@@ -15,6 +17,16 @@ import qualified Network.Wreq as W
 import System.Directory
 import qualified Data.Text as ST
 import Git
+import Data.ByteString (ByteString)
+import Data.Maybe
+import Data.Text.Encoding (encodeUtf8)
+import qualified Data.Text as T
+import Git.Libgit2 (MonadLg, LgRepo, lgFactory)
+import System.Process
+import System.IO
+import Prelude hiding (lines)
+import qualified Prelude as P
+import qualified Data.List.Extra as LE
 
 data CircleConfig = CircleConfig
   { apiToken :: Text
@@ -23,6 +35,12 @@ data CircleConfig = CircleConfig
 makeLenses ''CircleConfig
 
 instance Interpret CircleConfig
+
+splitByChar :: Char -> String -> [String]
+splitByChar c s = case rest of
+  []     -> [chunk]
+  _:rest -> chunk : splitByChar c rest
+  where (chunk, rest) = P.break (==c) s
 
 getMe :: W.Options -> IO ()
 getMe opts = do
@@ -40,8 +58,48 @@ setEnv opts = do
   r <- postWith opts "https://circleci.com/api/v1.1/project/github/pico-works/pico-disposal/envvar" (toJSON vars)
   print r
 
+data GithubRemote = GithubRemote
+  { githubRemoteOrganisation :: String
+  , githubRemoteProject      :: String
+  } deriving (Eq, Show)
+
+remoteEntriesFromLine :: String -> Maybe (String, GithubRemote)
+remoteEntriesFromLine s = case LE.split (== ' ') (LE.replace "\t" " " s) of
+  alias : uri : _ : xs -> case LE.split (== ':') uri of
+    [userHost, uriPath] -> if userHost == "git@github.com"
+      then case LE.split (== '/') uriPath of
+        [organisation, gitFile] -> case LE.split (== '.') gitFile of
+          [project, "git"] -> Just (alias, GithubRemote organisation project)
+          _ -> Nothing
+        _ -> Nothing
+      else Nothing
+  _ -> Nothing
+
 main :: IO ()
 main = do
+  remoteLines <- P.lines <$> readProcess "git" ["remote", "-v"] ""
+  let remoteEntries = LE.nub (remoteEntriesFromLine <$> remoteLines)
+  print remoteEntries
+
+  let repoOpts = RepositoryOptions
+        { repoPath = "." :: String
+        , repoWorkingDir = Nothing
+        , repoIsBare = False
+        , repoAutoCreate = False
+        }
+  repo <- openRepository lgFactory defaultRepositoryOptions { repoPath = "." }
+
+  withRepository lgFactory "." $ do
+    maybeRef <- lookupReference "HEAD"
+    case maybeRef of
+      Just ref -> do
+        oid <- referenceToOid ref
+        liftIO $ print "==="
+        liftIO $ print oid
+        liftIO $ print "==="
+      Nothing -> liftIO $ putStrLn "No ref"
+    return ()
+
   home <- pack <$> getHomeDirectory
   circleConfig <- input auto (home `append` "/.circle/config")
   print (circleConfig :: CircleConfig)
