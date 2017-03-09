d002b55b4fabb8b246d21a031d66e5e79de07608
John Ky <newhoggy@gmail.com>
2017-03-10 08:18:16 +1100

    Remove unused code

--- app/HaskellWorks/Ci/Action/FromRemote.hs
+++ app/HaskellWorks/Ci/Action/FromRemote.hs
@@ -4,31 +4,15 @@ module HaskellWorks.Ci.Action.FromRemote where
 
 import Data.Maybe
 import Data.Monoid
-import Data.Text.Lazy as LT
 import Data.Text.Lazy.IO as LTIO
-import HaskellWorks.Ci.Api.Circle
+import HaskellWorks.Ci.Git
 import HaskellWorks.Ci.Options
 import HaskellWorks.Ci.Options.Cmd.FromRemote
 import HaskellWorks.Ci.Text
-import HaskellWorks.Ci.Types
 import Prelude hiding (lines)
 import System.Process
 import qualified Data.List.Extra          as LE
-import qualified HaskellWorks.Ci.Options  as O
-import qualified Options.Applicative      as O
 import qualified Prelude                  as P
-
-remoteEntriesFromLine :: String -> Maybe (String, GithubRemote)
-remoteEntriesFromLine s = case LE.split (== ' ') (LE.replace "\t" " " s) of
-  alias : uri : _ : xs -> case LE.split (== ':') uri of
-    [userHost, uriPath] -> if userHost == "git@github.com"
-      then case LE.split (== '/') uriPath of
-        [organisation, gitFile] -> case LE.split (== '.') gitFile of
-          [project, "git"] -> Just (alias, GithubRemote organisation project)
-          _ -> Nothing
-        _ -> Nothing
-      else Nothing
-  _ -> Nothing
 
 actionFromRemote :: CmdFromRemote -> IO ()
 actionFromRemote cmd = do
