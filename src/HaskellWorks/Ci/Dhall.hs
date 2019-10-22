8b01bd88017d63be9c261f8e7899e259784f7332
John Ky <newhoggy@gmail.com>
2019-10-22 14:20:28 +1100

    Fix build

--- src/HaskellWorks/Ci/Dhall.hs
+++ src/HaskellWorks/Ci/Dhall.hs
@@ -5,9 +5,10 @@ module HaskellWorks.Ci.Dhall
   , auto
   ) where
 
-import Dhall as X hiding (auto)
+import Dhall        as X hiding (auto)
 import GHC.Generics
+
 import qualified Data.Text.Lazy as TL
 
 auto :: (GenericInterpret (Rep a), Generic a, Interpret a) => Type a
-auto = autoWith (defaultInterpretOptions { fieldModifier = TL.dropWhile (== '_') })
+auto = autoWith (defaultInterpretOptions { fieldModifier = TL.toStrict . TL.dropWhile (== '_') . TL.fromStrict })
